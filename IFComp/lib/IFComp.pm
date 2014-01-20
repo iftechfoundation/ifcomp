ec98ca6dab6815e2b445bb488bbb79e41bcbea3e
Joe Johnston <jjohn@taskboy.com>
2014-01-20 06:49:30 -0500

    Reworked code to remove Multi dependency

--- IFComp/lib/IFComp.pm
+++ IFComp/lib/IFComp.pm
@@ -18,7 +18,7 @@ use Catalyst::Runtime 5.80;
 
 use Catalyst qw/
     -Debug
-    ConfigLoader::Multi
+    ConfigLoader
     Static::Simple
 /;
 
@@ -40,7 +40,7 @@ __PACKAGE__->config(
     # Disable deprecated behavior needed by old applications
     disable_component_resolution_regex_fallback => 1,
     enable_catalyst_header => 1, # Send X-Catalyst header
-    'Plugin::ConfigLoader' => {file => __PACKAGE__->path_to("conf") }, # Load configs from the conf dir
+    'Plugin::ConfigLoader' => {file => "conf/ifcomp_local.conf" }, # Load configs from the conf dir
 );
 
 # Start the application
