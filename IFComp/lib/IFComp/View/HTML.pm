96da3320ae8eaf11992f1e87d5e18c660a54d241
Jason McIntosh <jmac@jmac.org>
2014-01-26 19:22:51 -0500

    Adding various files generated by Catalyst::Helper::View::Bootstrap.

---
+++ IFComp/lib/IFComp/View/HTML.pm
@@ -0,0 +1,42 @@
+package IFComp::View::HTML;
+
+use strict;
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config({
+    INCLUDE_PATH => [
+        IFComp->path_to( 'root', 'src' ),
+        IFComp->path_to( 'root', 'lib' )
+    ],
+    PRE_PROCESS  => 'config/main',
+    WRAPPER      => 'site/wrapper',
+    ERROR        => 'error.tt2',
+    TIMER        => 0,
+    render_die   => 1,
+});
+
+=head1 NAME
+
+IFComp::View::HTML - Catalyst TT Twitter Bootstrap View
+
+=head1 SYNOPSIS
+
+See L<IFComp>
+
+=head1 DESCRIPTION
+
+Catalyst TTSite View.
+
+=head1 AUTHOR
+
+Jason McIntosh
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
+
