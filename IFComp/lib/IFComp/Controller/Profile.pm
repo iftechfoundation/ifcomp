f2ad782db11a943b0b99dd0770318f0e18a9e8f9
Joe Johnston <jjohn@taskboy.com>
2014-02-11 21:43:36 -0500

    Basic Auth.pm structure in place, but need to work on implementation. TryCatch not working as expected. Work in Progress

--- IFComp/lib/IFComp/Controller/Profile.pm
+++ IFComp/lib/IFComp/Controller/Profile.pm
@@ -4,6 +4,8 @@ use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller'; }
 
+use JSON::Any;
+
 =head1 NAME
 
 IFComp::Controller::Profile - Catalyst Controller
@@ -21,13 +23,76 @@ Catalyst Controller.
 
 =cut
 
-sub index :Path :Args(0) {
+sub index :Path :Args(0) 
+{
     my ( $self, $c ) = @_;
 
     $c->response->body('Matched IFComp::Controller::Profile in Profile.');
 }
 
 
+=head2 auth.php
+
+You read that correctly.  This is the endpoint that supports legacy federated logins.
+
+=cut
+
+sub auth :Path('auth.php')
+{
+    my ( $self, $c ) = @_;
+    my %supported_actions = (
+                             "login" => \&auth_login,
+                             "verfiy" => \&auth_verify,
+                             "logout" => \&auth_logout,
+                            );
+    my $type = $c->request->param("type");
+
+    my $data;
+    if (exists $supported_actions{$type})
+    {
+        # FIXME:  Try block is propagating exception
+        try 
+        { 
+            $data = $supported_actions{$type}->($self, $c) 
+        }
+        catch 
+        {
+            $data = {
+                error_code => "ERROR",
+                error_text => $@,
+            };
+        }
+
+    }
+    else
+    {
+        $data = {
+            error_code => "UNKNOWN ACTION",
+            error_text => "Do not know how to '$type'",
+        };
+    }
+
+    $c->response->content_type("application/json");
+    $c->response->body(encode_json($data));
+}
+
+sub auth_login
+{
+    my ($self, $c) = @_;
+    die("NYI\n");
+}
+
+sub auth_verify
+{
+    my ($self, $c) = @_;
+    die("NYI\n");
+}
+
+sub auth_logout
+{
+    my ($self, $c) = @_;
+    die("NYI\n");
+}
 
 =encoding utf8
 
