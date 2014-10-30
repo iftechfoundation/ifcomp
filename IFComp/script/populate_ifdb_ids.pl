c5651f04654b35dffd6dd920a588365b06ccc09a
Jason McIntosh <jmac@jmac.org>
2014-10-30 16:42:32 -0400

    Adding a script to populate the current comp entrys' ifdb_id fields.

---
+++ IFComp/script/populate_ifdb_ids.pl
@@ -0,0 +1,55 @@
+#!/usr/env perl
+
+# This script does its best to update the IFDB ID field for every game in the
+# current year's comp.
+
+use warnings;
+use strict;
+use FindBin;
+use Path::Class;
+use LWP;
+use Readonly;
+
+use lib "$FindBin::Bin/../lib";
+use IFComp::Schema;
+
+Readonly my $PAUSE_BETWEEN_QUERIES => 1;
+
+my $schema = IFComp::Schema->connect( 'dbi:mysql:ifcomp', 'root', '' );
+$schema->entry_directory( Path::Class::Dir->new( "$FindBin::Bin/../entries" ) );
+
+my $current_comp = $schema->resultset( 'Comp' )->current_comp;
+
+my $ua = LWP::UserAgent->new;
+
+
+for my $entry ( $current_comp->entries ) {
+
+    next unless $entry->is_qualified;
+
+    my $title = $entry->title;
+    my $year = $current_comp->year;
+
+    my $query_string = "$title published:$year";
+    $query_string =~ s/ /+/g;
+    my $uri = "http://ifdb.tads.org/search?xml&game&searchfor=$query_string";
+
+    print "$uri\n\n";
+
+    my $response = $ua->get( $uri );
+
+    my ( $ifdb_id ) = $response->content =~ m{<tuid>([\w\d]+?)</tuid><title>\s*$title\s*</title>}i;
+    if ( $ifdb_id ) {
+        $entry->ifdb_id( $ifdb_id );
+        $entry->update;
+        warn "$title has the IFDB $ifdb_id\n";
+    }
+    else {
+        warn "WARNING: Couldn't find an IFDB ID for $title.\n";
+    }
+
+    sleep( $PAUSE_BETWEEN_QUERIES );
+}
+
+
+
