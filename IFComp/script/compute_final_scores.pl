56b7900382ebc0b842ed72cb9a25df077c39f3c5
Jason McIntosh <jmac@jmac.org>
2014-11-15 17:34:23 -0500

    The score-computing script extends the new tie-reckoning logic to miss congeniality, and also cleans up old results before running.

--- IFComp/script/compute_final_scores.pl
+++ IFComp/script/compute_final_scores.pl
@@ -19,13 +19,16 @@ $schema->entry_directory( Path::Class::Dir->new( "$FindBin::Bin/../entries" ) );
 
 my $current_comp = $schema->resultset( 'Comp' )->current_comp;
 
-my $ifcomp_sql = 'select entry.id as entry_id, avg(score) as average_score, std(score) as standard_deviation, count(score) as votes_cast, sum(score = 1) as total_1, sum(score = 2) as total_2, sum(score = 3) as total_3, sum(score = 4) as total_4, sum(score = 5) as total_5, sum(score = 6) as total_6, sum(score = 7) as total_7, sum(score = 8) as total_8, sum(score = 9) as total_9, sum(score = 10) as total_10 from ( select vote.* from vote left join entry on vote.user = entry.author and entry.comp = ? where (entry.author is null or entry.is_disqualified) and user in ( select user from vote, entry where entry.id = vote.entry and entry.comp = 24 group by user having count(score) >= 5 ) ) judge_vote, entry where entry.id = judge_vote.entry group by entry.id order by avg(score) desc';
+my $ifcomp_sql = 'select entry.id as entry_id, avg(score) as average_score, std(score) as standard_deviation, count(score) as votes_cast, sum(score = 1) as total_1, sum(score = 2) as total_2, sum(score = 3) as total_3, sum(score = 4) as total_4, sum(score = 5) as total_5, sum(score = 6) as total_6, sum(score = 7) as total_7, sum(score = 8) as total_8, sum(score = 9) as total_9, sum(score = 10) as total_10 from ( select vote.* from vote left join entry on vote.user = entry.author and entry.comp = ? where (entry.author is null or entry.is_disqualified) and user in ( select user from vote, entry where entry.id = vote.entry and entry.comp = ? group by user having count(score) >= 5 ) ) judge_vote, entry where entry.id = judge_vote.entry group by entry.id order by avg(score) desc';
 my $mc_sql = 'select entry.id as entry_id, avg(score) as average_score from ( select vote.* from vote left join entry on vote.user = entry.author and entry.comp = ? where entry.is_disqualified = 0 ) judge_vote, entry where entry.id = judge_vote.entry group by entry.id order by average_score desc';
 
 my $dbh = $schema->storage->dbh;
 
+$dbh->do( 'update entry set place = NULL where comp = ?', {}, $current_comp->id );
+$dbh->do( 'update entry set miss_congeniality_place = NULL where comp = ?', {}, $current_comp->id );
+
 my $ifcomp_sth = $dbh->prepare( $ifcomp_sql );
-$ifcomp_sth->execute( $current_comp->id );
+$ifcomp_sth->execute( $current_comp->id, $current_comp->id );
 
 my $current_place;
 my $entry_count;
@@ -55,6 +58,7 @@ my $mc_sth = $dbh->prepare( $mc_sql );
 $mc_sth->execute( $current_comp->id );
 
 undef $current_place;
+undef $entry_count;
 undef $previous_average;
 while ( my $row_ref = $mc_sth->fetchrow_hashref ) {
     my $entry = $schema->resultset( 'Entry' )->find( $row_ref->{ entry_id } );
@@ -63,10 +67,10 @@ while ( my $row_ref = $mc_sth->fetchrow_hashref ) {
         ( not defined $previous_average )
         || ( $previous_average > $row_ref->{ average_score } )
     ) {
-        $current_place++;
+        $current_place = ++$entry_count;
     }
 
-    if ( $current_place > $MAX_MC_PLACE ) {
+    if ( $entry_count > $MAX_MC_PLACE ) {
         last;
     }
     else {
