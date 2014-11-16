0ee08b93f8a76cb5f7c712cd94c3bbe2f45a0dd0
Jason McIntosh <jmac@jmac.org>
2014-11-16 12:21:17 -0500

    Storing scores & standard deviations to 2 significant digits, not 4.

--- IFComp/script/compute_final_scores.pl
+++ IFComp/script/compute_final_scores.pl
@@ -19,8 +19,8 @@ $schema->entry_directory( Path::Class::Dir->new( "$FindBin::Bin/../entries" ) );
 
 my $current_comp = $schema->resultset( 'Comp' )->current_comp;
 
-my $ifcomp_sql = 'select entry.id as entry_id, avg(score) as average_score, std(score) as standard_deviation, count(score) as votes_cast, sum(score = 1) as total_1, sum(score = 2) as total_2, sum(score = 3) as total_3, sum(score = 4) as total_4, sum(score = 5) as total_5, sum(score = 6) as total_6, sum(score = 7) as total_7, sum(score = 8) as total_8, sum(score = 9) as total_9, sum(score = 10) as total_10 from ( select vote.* from vote left join entry on vote.user = entry.author and entry.comp = ? where (entry.author is null or entry.is_disqualified) and user in ( select user from vote, entry where entry.id = vote.entry and entry.comp = ? group by user having count(score) >= 5 ) ) judge_vote, entry where entry.id = judge_vote.entry group by entry.id order by avg(score) desc';
-my $mc_sql = 'select entry.id as entry_id, avg(score) as average_score from ( select vote.* from vote left join entry on vote.user = entry.author and entry.comp = ? where entry.is_disqualified = 0 ) judge_vote, entry where entry.id = judge_vote.entry group by entry.id order by average_score desc';
+my $ifcomp_sql = 'select entry.id as entry_id, round(avg(score), 2) as average_score, round(std(score), 2) as standard_deviation, count(score) as votes_cast, sum(score = 1) as total_1, sum(score = 2) as total_2, sum(score = 3) as total_3, sum(score = 4) as total_4, sum(score = 5) as total_5, sum(score = 6) as total_6, sum(score = 7) as total_7, sum(score = 8) as total_8, sum(score = 9) as total_9, sum(score = 10) as total_10 from ( select vote.* from vote left join entry on vote.user = entry.author and entry.comp = ? where (entry.author is null or entry.is_disqualified) and user in ( select user from vote, entry where entry.id = vote.entry and entry.comp = ? group by user having count(score) >= 5 ) ) judge_vote, entry where entry.id = judge_vote.entry group by entry.id order by avg(score) desc';
+my $mc_sql = 'select entry.id as entry_id, round(avg(score), 2) as average_score from ( select vote.* from vote left join entry on vote.user = entry.author and entry.comp = ? where entry.is_disqualified = 0 ) judge_vote, entry where entry.id = judge_vote.entry group by entry.id order by average_score desc';
 
 my $dbh = $schema->storage->dbh;
 
@@ -34,6 +34,7 @@ my $current_place;
 my $entry_count;
 my $previous_average;
 while ( my $row_ref = $ifcomp_sth->fetchrow_hashref ) {
+    $entry_count++;
     my $entry = $schema->resultset( 'Entry' )->find( $row_ref->{ entry_id } );
 
     for my $field ( keys %$row_ref ) {
@@ -46,7 +47,7 @@ while ( my $row_ref = $ifcomp_sth->fetchrow_hashref ) {
         ( not defined $previous_average )
         || ( $previous_average > $row_ref->{ average_score } )
     ) {
-        $current_place = ++$entry_count;
+        $current_place = $entry_count;
     }
     $entry->place( $current_place );
     $entry->update;
@@ -61,13 +62,14 @@ undef $current_place;
 undef $entry_count;
 undef $previous_average;
 while ( my $row_ref = $mc_sth->fetchrow_hashref ) {
+    $entry_count++;
     my $entry = $schema->resultset( 'Entry' )->find( $row_ref->{ entry_id } );
 
     if (
         ( not defined $previous_average )
         || ( $previous_average > $row_ref->{ average_score } )
     ) {
-        $current_place = ++$entry_count;
+        $current_place = $entry_count;
     }
 
     if ( $entry_count > $MAX_MC_PLACE ) {
