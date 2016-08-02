3674f9f3686c19cebce10a24cd84eae6adba2eb8
Jason McIntosh <jmac@jmac.org>
2016-08-02 17:33:42 -0400

    Checking for user-definedness before subjecting the user object to tests.

--- IFComp/lib/Plack/Middleware/IFComp.pm
+++ IFComp/lib/Plack/Middleware/IFComp.pm
@@ -93,11 +93,13 @@ sub call {
                         my $user =
                             $self->schema->resultset( 'User' )->find( $user_id )
                         ;
-                        if ( $user_id == $entry->author->id ) {
-                            $current_user_can_see_this_game = 1;
-                        }
-                        elsif ( grep { $_->name eq 'curator' } $user->roles->all ) {
-                            $current_user_can_see_this_game = 1;
+                        if ( defined $user ) {
+                            if ( $user_id == $entry->author->id ) {
+                                $current_user_can_see_this_game = 1;
+                            }
+                            elsif ( grep { $_->name eq 'curator' } $user->roles->all ) {
+                                $current_user_can_see_this_game = 1;
+                            }
                         }
                     }
                 }
