293a0b30a71ed6724f3416cd301fe31470397733
Jason McIntosh <jmac@jmac.org>
2016-10-21 22:18:24 -0400

    Whatever es6-shim is, either it or something else in the big minified JS after its first mention in Twine games doesn't like being encoded into UTF-8. Here's a horrible hack until we figure it out better. (Or, let's face it, until it breaks again and then we have to look deeper.)

--- IFComp/lib/IFComp/Controller/Play.pm
+++ IFComp/lib/IFComp/Controller/Play.pm
@@ -70,8 +70,22 @@ sub download :Chained('fetch_entry') :Args(0) {
     my $filename = $entry->main_file->basename;
     if ( $entry->platform eq 'html' ) {
         my $body = $entry->main_file->slurp(iomode => '<:encoding(UTF-8)');
+        # XXX Horrible hack to get around a thing in Twine-generated files
+        #     that I don't understand yet.
+        if ( $body =~ /es6-shim/ ) {
+            # It's a Twine file that uses a 'es6-shim', whatever that is.
+            # Well, it doesn't play nice with UTF-8, I guess?
+            # Re-slurp without UTF-8 encoding.
+            my $non_utf8 = $entry->main_file->slurp;
+            # Replace everything in the body from the first mention of
+            # 'es6-shim' onwards with poorly-encoded version of same.
+            # Else the page will fail to load. No, I don't know either.
+            my ( $shim_text ) = $non_utf8 =~ /(es6-shim.*)$/s;
+            $body =~ s/es6-shim.*//s;
+            $body .= $shim_text;
+        }
         $c->res->header( 'Content-Disposition' => "attachment; filename=$filename" );
-        $c->res->content_type( 'text/html; charset=utf-8' );
+        $c->res->content_type( 'text/html' );
         $c->res->code( 200 );
         $c->res->body( $body );
     }
