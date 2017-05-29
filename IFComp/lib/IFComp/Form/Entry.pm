780c1986ae4fc1473b08a90bd2a6179932697ce2
Jason McIntosh <jmac@jmac.org>
2017-05-29 15:45:31 -0400

    Fixes #84.

--- IFComp/lib/IFComp/Form/Entry.pm
+++ IFComp/lib/IFComp/Form/Entry.pm
@@ -32,6 +32,7 @@ has_field 'playtime' => (
     type => 'Select',
     label => 'Estimated play time',
     empty_select => '',
+    id => 'playtime',
     options => [[
         '15 minutes or less',
         'half an hour',
