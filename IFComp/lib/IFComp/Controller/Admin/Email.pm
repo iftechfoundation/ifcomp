25f20215ecd950caec95aa5d99bc1a9eef745210
Jason McIntosh <jmac@jmac.org>
2017-09-02 15:16:04 -0400

    tidyall

--- IFComp/lib/IFComp/Controller/Admin/Email.pm
+++ IFComp/lib/IFComp/Controller/Admin/Email.pm
@@ -23,25 +23,23 @@ sub index : Chained("/admin/root") : PathPart('email') : Args(0) {
     } @disqualified_emails;
 
     $c->stash(
-        emails => \@emails,
+        emails      => \@emails,
         anti_emails => \@anti_emails,
     );
 
 }
 
 sub _get_emails {
-    my ($c, $comp, $is_disqualified) = @_;
-    my @emails = $c->model( 'IFCompDB::User' )->search(
-        {
-            'entries.comp' => $comp->id,
+    my ( $c, $comp, $is_disqualified ) = @_;
+    my @emails = $c->model('IFCompDB::User')->search(
+        {   'entries.comp'            => $comp->id,
             'entries.is_disqualified' => $is_disqualified,
         },
-        {
-            join => 'entries',
+        {   join     => 'entries',
             group_by => 'email',
             order_by => 'email',
         },
-    )->get_column( 'email' )->all;
+    )->get_column('email')->all;
 
     return @emails;
     die "Got this: @emails";
