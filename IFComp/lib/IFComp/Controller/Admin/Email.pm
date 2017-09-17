27d49c2ac5e661c9422bc7eaa0b36c736a34dde4
Jason McIntosh <jmac@jmac.org>
2017-09-17 13:47:07 -0400

    Refactoring email-fetching logic out of the controller and into the schema.

--- IFComp/lib/IFComp/Controller/Admin/Email.pm
+++ IFComp/lib/IFComp/Controller/Admin/Email.pm
@@ -2,8 +2,6 @@ package IFComp::Controller::Admin::Email;
 use Moose;
 use namespace::autoclean;
 
-use List::Util qw(none);
-
 BEGIN { extends 'Catalyst::Controller'; }
 
 sub index : Chained("/admin/root") : PathPart('email') : Args(0) {
@@ -11,16 +9,9 @@ sub index : Chained("/admin/root") : PathPart('email') : Args(0) {
 
     my $comp = $c->model('IFCompDB::Comp')->current_comp;
 
-    my @emails = _get_emails( $c, $comp, 0 );
-
-    my @disqualified_emails = _get_emails( $c, $comp, 1 );
-
-    # The "anti-email" list is all holders of disqualified games that
-    # don't also hold qualified games.
-    my @anti_emails = grep {
-        my $dq_email = $_;
-        none { $_ eq $dq_email } @emails;
-    } @disqualified_emails;
+    my $schema = $c->model('IFCompDB')->schema;
+    my @emails = $schema->emails_for_comp( $comp );
+    my @anti_emails = $schema->anti_emails_for_comp( $comp );
 
     $c->stash(
         emails      => \@emails,
@@ -42,7 +33,6 @@ sub _get_emails {
     )->get_column('email')->all;
 
     return @emails;
-    die "Got this: @emails";
 }
 
 __PACKAGE__->meta->make_immutable;
--- IFComp/lib/IFComp/Schema.pm
+++ IFComp/lib/IFComp/Schema.pm
@@ -17,6 +17,7 @@ __PACKAGE__->load_namespaces;
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:uxXflUCGtsGN5LDbi7A+7A
 
 use MooseX::ClassAttribute;
+use List::Util qw(none);
 
 class_has 'email_template_basedir' => (
     is  => 'rw',
@@ -27,6 +28,46 @@ class_has 'entry_directory' => (
     is  => 'rw',
     isa => 'Path::Class::Dir',
 );
+
+sub emails_for_comp {
+    my $self = shift;
+
+    return $self->_get_emails( @_, 0 );
+
+}
+
+sub anti_emails_for_comp {
+    my $self = shift;
+
+    # The "anti-email" list is all holders of disqualified games that
+    # don't also hold qualified games.
+    my @emails = $self->_get_emails( @_, 0 );
+    my @disqualified_emails = $self->_get_emails( @_, 1 );
+    my @anti_emails = grep {
+        my $dq_email = $_;
+        none { $_ eq $dq_email } @emails;
+    } @disqualified_emails;
+
+    return @anti_emails;
+}
+
+sub _get_emails {
+    my $self = shift;
+    my ( $comp, $is_disqualified ) = @_;
+
+    my @emails = $self->resultset('User')->search(
+        {   'entries.comp'            => $comp->id,
+            'entries.is_disqualified' => $is_disqualified,
+        },
+        {   join     => 'entries',
+            group_by => 'email',
+            order_by => 'email',
+        },
+    )->get_column('email')->all;
+
+    return @emails;
+
+}
 
 __PACKAGE__->meta->make_immutable( inline_constructor => 0 );
 1;
