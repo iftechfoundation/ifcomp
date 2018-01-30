0e80bb594f87edd71863b401c0bea99b3155c285
Jason McIntosh <jmac@jmac.org>
2018-01-30 16:14:42 -0500

    Hotfix: If the current comp hasn't started yet, then the prize-list page will display last year's prizes (if available).

--- IFComp/lib/IFComp/Controller/About.pm
+++ IFComp/lib/IFComp/Controller/About.pm
@@ -66,6 +66,15 @@ sub prizes : Path('prizes') : Args(0) {
     my ( $self, $c ) = @_;
 
     my $current_comp = $c->model('IFCompDB::Comp')->current_comp;
+
+    # If this year's comp hasn't started yet, let's show last year's prizes
+    # instead.
+    # (Unless there is no last year!)
+    if ( $current_comp->status eq 'not_begun' ) {
+        my $last_comp = $c->model('IFCompDB::Comp')->last_comp;
+        $current_comp = $last_comp if $last_comp;
+    }
+
     $c->stash->{current_comp} = $current_comp;
 
     my %prizes_in_category;
