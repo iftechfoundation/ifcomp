3eca347aa4bd2be04654f03d814b3971ec4346e5
Adam Herzog <adam@herzogsc.com>
2021-05-26 16:09:24 -0400

    Copy test files into dev docker env.

--- IFComp-Dev/docker-app/docker-script/load_test_data.pl
+++ IFComp-Dev/docker-app/docker-script/load_test_data.pl
@@ -2,6 +2,8 @@
 use strict;
 use warnings;
 
+use v5.10;
+
 use lib ("/opt/IFComp/lib");
 use lib ("/opt/IFComp/t/lib");
 
@@ -9,4 +11,12 @@ use IFComp;
 use IFCompTestData;
 
 my $schema = IFComp->component("IFComp::Model::IFCompDB")->schema;
+
+say "Adding test data to schema";
 IFCompTestData->add_test_data_to_schema($schema);
+
+say "Copying test files into place";
+IFCompTestData->copy_test_files(
+    "/opt/IFComp/t/test_files/entries" => "/opt/IFComp/entries" );
+
+say "Done";
