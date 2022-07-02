7034ca60294bff11bd8dd30f3209e7778c84648a
Dannii Willis <curiousdannii@gmail.com>
2022-07-02 22:49:27 +1000

    Update Parchment interpreters (#362)

--- IFComp/root/static/interpreter/ie.js
+++ IFComp/root/static/interpreter/ie.js
@@ -1,1 +1,2 @@
-$((function(){$("#errorcontent").text("Sorry, but Parchment depends on web features unsupported by this browser. Please try a more modern browser."),$("#errorpane").show()}));
+function r(){$("#errorcontent").text("Sorry, but Parchment depends on web features unsupported by this browser. Please try a more modern browser."),$("#errorpane").show()}$(r);
+//# sourceMappingURL=ie.js.map
