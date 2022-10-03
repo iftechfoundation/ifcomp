28faa8870e35ca9c442b5a7cdacb955013d7e1f8
Mark Musante <mark.musante@gmail.com>
2022-10-03 17:33:21 -0400

    tidy

--- IFComp/script/create_full_game_directory.pl
+++ IFComp/script/create_full_game_directory.pl
@@ -9,74 +9,70 @@ use lib "$FindBin::Bin/../lib";
 use IFComp::Schema;
 
 my $schema = IFComp::Schema->connect( 'dbi:mysql:ifcomp', 'root', '' );
-$schema->entry_directory( Path::Class::Dir->new( "$FindBin::Bin/../entries" ) );
+$schema->entry_directory( Path::Class::Dir->new("$FindBin::Bin/../entries") );
 
-my ( $source_path ) = @ARGV;
+my ($source_path) = @ARGV;
 
 die "Usage: $0 destination_directory\n" unless $source_path;
 
-my $root = Path::Class::Dir->new( $source_path );
+my $root = Path::Class::Dir->new($source_path);
 
-unless (-d $root && -w $root) {
+unless ( -d $root && -w $root ) {
     die "Can't write to $root: $!\n";
 }
 
 my $count = 0;
 
-for my $entry (
-    $schema->resultset( 'Comp' )->current_comp->entries->all
-) {
+for my $entry ( $schema->resultset('Comp')->current_comp->entries->all ) {
     say "Opening up " . $entry->title;
     next unless $entry->is_qualified;
     print sprintf "I see %s by %s, a %s.\n",
-    $entry->title,
-    $entry->author->name,
-    $entry->platform,
-    ;
+        $entry->title,
+        $entry->author->name,
+        $entry->platform,
+        ;
 
     $count++;
 
     my $title = $entry->title;
 
     # Remove all diacritics.
-    $title = NFKD( $title );
+    $title = NFKD($title);
     $title =~ s/\p{NonspacingMark}//g;
 
     # Kill all non-word characters remaining, because Windows is stupid.
     $title =~ s/[^\w\d\s]//g;
     $title =~ s/ +/ /g;
 
-    my $destination = $root->subdir( $title );
+    my $destination = $root->subdir($title);
     if ( -e $destination ) {
-	$destination->rmtree;
+        $destination->rmtree;
     }
 
     $destination->mkpath;
     my $main_file = $destination->file( $entry->main_file->basename );
 
-    $entry->main_file->copy_to( $main_file );
+    $entry->main_file->copy_to($main_file);
     if ( $main_file->basename =~ /\.zip$/i ) {
         my $zip = Archive::Zip->new;
         $zip->read( $main_file->stringify );
-        eval {
-        $zip->extractTree( { zipName => $destination } );
-        };
+        eval { $zip->extractTree( { zipName => $destination } ); };
         if ($@) {
             warn "SKIPPING: $@\n";
         }
-	$main_file->remove;
+        $main_file->remove;
     }
 
     if ( $entry->cover_file ) {
-	my $subdir = $destination->subdir( 'Cover' );
-	$subdir->mkpath;
-	$entry->cover_file->copy_to( $subdir );
+        my $subdir = $destination->subdir('Cover');
+        $subdir->mkpath;
+        $entry->cover_file->copy_to($subdir);
     }
 
     if ( $entry->walkthrough_file ) {
-	my $subdir = $destination->subdir( 'Walkthrough' );
-	$subdir->mkpath;
-	$entry->walkthrough_file->copy_to( $subdir );
+        my $subdir = $destination->subdir('Walkthrough');
+        $subdir->mkpath;
+        $entry->walkthrough_file->copy_to($subdir);
     }
 
 }
