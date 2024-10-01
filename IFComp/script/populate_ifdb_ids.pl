b0e05ee9ed2663f4b4c4c3cedb69e67289cdbb12
Dan Fabulich <dan@fabulich.com>
2024-10-01 18:40:45 -0400

    populate_ifdb_ids: Search IFDB by `competitionid:`, match by entry ID (#437)
    Instead of searching for games by title published during the current calendar year, it'll be more accurate to search IFDB for the current year's competition, then search for games with that competitionid. For each game, we find a link to its IFComp ballot entry, which contains a ballot entry ID.

--- IFComp/script/populate_ifdb_ids.pl
+++ IFComp/script/populate_ifdb_ids.pl
@@ -13,47 +13,95 @@ use Readonly;
 use lib "$FindBin::Bin/../lib";
 use IFComp::Schema;
 
+use XML::LibXML;
+
 use open ':std', ':encoding(UTF-8)';
 
 Readonly my $PAUSE_BETWEEN_QUERIES => 1;
 
-my $schema = IFComp::Schema->connect( 'dbi:mysql:ifcomp', 'root', '',
-    { mysql_enable_utf8 => 1 } );
+my $schema = IFComp::Schema->connect( 'dbi:mysql:ifcomp',
+    'root', '', { mysql_enable_utf8 => 1 } );
 $schema->entry_directory( Path::Class::Dir->new("$FindBin::Bin/../entries") );
 
 my $current_comp = $schema->resultset('Comp')->current_comp;
 
 my $ua = LWP::UserAgent->new;
 
+my $year = $current_comp->year;
+
+my $ifdb_url = "https://ifdb.org";
+
+# my $ifdb_url = "http://localhost:8083/";
+
+# Search for the current year's competition in the series:Annual Interactive Fiction Competition
+
+my $competition_search_year_uri =
+    "$ifdb_url/search?comp&searchfor=$year%20series%3AAnnual+Interactive+Fiction+Competition&xml";
+
+my $competition_tuid = @{ XML::LibXML->load_xml(
+        string => $ua->get($competition_search_year_uri)->content
+    )->getElementsByTagName("tuid")
+}[0]->textContent;
+
+print "competition_tuid: $competition_tuid\n\n";
+
+sleep($PAUSE_BETWEEN_QUERIES);
+
+# Search for all games with that competitionid
+
+my $competition_search_uri =
+    "$ifdb_url/search?searchfor=competitionid:$competition_tuid&pg=all&sortby=ttl&xml";
+
+my @games = XML::LibXML->load_xml(
+    string => $ua->get($competition_search_uri)->content )
+    ->getElementsByTagName("game");
+
+my @tuids =
+    map { @{ $_->getElementsByTagName('tuid') }[0]->textContent } @games;
+
+sub entry_for_tuid {
+    my $tuid = shift;
+
+    # load the ifiction (XML) for the game
+    my $ifiction_uri = "$ifdb_url/viewgame?id=$tuid&ifiction";
+
+    sleep($PAUSE_BETWEEN_QUERIES);
+
+    # Find all the external links
+    my @links =
+        @{ XML::LibXML->load_xml( string => $ua->get($ifiction_uri)->content )
+            ->getElementsByTagName("links") }[0]
+        ->getElementsByTagName("link");
+
+    # Find the link whose URL looks like a ballot link, containing an IFComp entry ID
+    foreach my $link (@links) {
+        my $url = @{ $link->getElementsByTagName("url") }[0]->textContent;
+        if ( $url =~ /^https:\/\/ifcomp.org\/ballot\/#entry-(\d+)$/ ) {
+            my $entry_id = $1;
+            print "entry_id $entry_id => $tuid\n";
+            return $1;
+        }
+    }
+}
+
+my %entries = map { entry_for_tuid($_) => $_ } @tuids;
+
 for my $entry ( $current_comp->entries ) {
 
     next unless $entry->is_qualified;
 
-    my $title = $entry->title;
-    my $year  = $current_comp->year;
+    my $entry_id = $entry->id;
+    my $title    = $entry->title;
+    my $ifdb_id  = $entries{ $entry->id };
 
-    my $query_string = "$title published:$year";
-    $query_string =~ s/ /+/g;
-    my $uri = "https://ifdb.org/search?xml&game&searchfor=$query_string";
-
-    print "$uri\n\n";
-
-    my $response = $ua->get($uri);
-
-    my $escaped_title = quotemeta $title;
-    my ($ifdb_id) = $response->content
-        =~ m{<tuid>([\w\d]+?)</tuid><title>\s*$escaped_title\s*</title>}i;
     if ($ifdb_id) {
         $entry->ifdb_id($ifdb_id);
         $entry->update;
-        warn "$title has the IFDB $ifdb_id\n";
+        warn "$title ($entry_id) has the IFDB $ifdb_id\n";
     }
     else {
-        my $entry_id = $entry->id;
         warn
             "*** WARNING: Couldn't find an IFDB ID for $title ($entry_id).\n";
     }
-
-    sleep($PAUSE_BETWEEN_QUERIES);
 }
 
